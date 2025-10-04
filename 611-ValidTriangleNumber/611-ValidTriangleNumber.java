80578bfca0d50c8a03121db1f8f18f692ac7e9ff
M Sai Vipul Chinmay <jarvis0y2t1b9u5n@gmail.com>
2025-10-04 15:52:27 +0530

    Create 611-ValidTriangleNumber

---
+++ 611-ValidTriangleNumber/611-ValidTriangleNumber.java
@@ -0,0 +1,21 @@
+// Last updated: 04/10/2025, 15:52:26
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        Arrays.sort(nums);
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += j - i; 
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+}
