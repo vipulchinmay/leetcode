9e1e3c2060a876aaccfd66cbdaa56f0f6806ec34
M Sai Vipul Chinmay <jarvis0y2t1b9u5n@gmail.com>
2025-10-05 00:57:08 +0530

    Create

---
+++ problem-1759606027912/problem-1759606027912.java
@@ -0,0 +1,26 @@
+// Last updated: 05/10/2025, 00:57:07
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(slow!=null && fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
